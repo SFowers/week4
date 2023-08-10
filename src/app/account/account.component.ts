f2ad887eef2a61e44376a1ed3b111d761c3dec9c
Sean Fowers <sean.fowers@griffithuni.edu.au>
2023-08-10 17:00:47 +1000

    Week 4 Tutorial setup

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.scss']
+})
+export class AccountComponent implements OnInit {
+
+  email:string = "";
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    //this.email = this.route.snapshot.params.email;
+    /*
+    this.route.paramMap.subscribe(
+      params => {this.email = params.get('email');}
+    );
+    */
+  }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  email:string = "";
+  password:string = "";
+
+  users = [{email:'abc@gmail.com', password:'123'}, 
+          {email:'def@gmail.com', password:'123'}, 
+          {email:'ghi@gmail.com', password:'123'}];
+
+  constructor(private router: Router) {}
+
+  login() {
+    var confirmedUser = 0;
+    for(let i = 0; i < 3; i++) {
+      if(this.users[i].email == this.email && this.users[i].password == this.password) {
+        confirmedUser = 1;
+      }
+    }
+    if(confirmedUser) {
+      this.router.navigateByUrl('/account' );
+    } else {
+      alert("Error: Login Details do not match.");
+    }
+    
+  }
+}
